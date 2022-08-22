27181a588ce7c05e8fb9be853983fa60b4d273c5
musrafulhaq <musrafulhaque.kat@gmail.com>
2022-08-22 22:53:34 +0530

    Again changes in code

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return '<h3>This is a Flask web application.</h3>'
-    
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/index/')
+def hello():
+    return '<h1>Hello, World!</h1>'
 
+@app.route('/about/')
+def about():
+    return '<h3>This is a Flask web application.</h3>'
